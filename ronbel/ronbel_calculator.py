db087b43cd83609f878b38747691cad52780234c
Andrew <sgtsh4ksw@gmail.com>
2024-10-31 18:01:42 +0300

    feat: (ronbell) add method signatures

--- ronbel/ronbel_calculator.py
+++ ronbel/ronbel_calculator.py
@@ -1,4 +1,26 @@
 #TODO: Box
-
+#                                                                            0427       300      300     100       t24              bury                                420
 _REQUEST_URL_TEMPLATE: str = "https://app.ronbel.ru/korobka/client/samovyvoz/{box_type}/{length}/{width}/{height}/{cardboard_type}/{color}/print_no/options_no/price/{quantity}"
 
+def calculate():
+    #Проверить входные параметры
+    #Подогнать входные параметры под вид для запроса
+    #Сделать запрос по урл
+    #Спарсить со страницы ответ
+    #Упаковать ответ в структуру Box
+    pass
+
+def _check_input_parameters():
+    pass
+
+def _adjust_parameters_for_url_format():
+    pass
+
+def _send_request():
+    pass
+
+def _parse_response():
+    pass
+
+def _adjust_response_parameters_format():
+    pass
