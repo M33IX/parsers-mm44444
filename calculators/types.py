e450eb0fa9e60d4290a91d5696d6a61a4c0faf1f
Andrew <sgtsh4ksw@gmail.com>
2024-10-31 18:51:00 +0300

    ref: move types to separate module

---
+++ calculators/types.py
@@ -0,0 +1,29 @@
+from typing import Literal, NamedTuple, Dict
+
+type BoxType = Literal[427] | Literal[201]
+type ColorType = Literal["бурый"] | Literal["белый"]
+type CardboardType = Literal["T-23", "T-24"]
+
+type CardboardPriceMatrix = Dict[ColorType, Dict[CardboardType, float]]
+
+class Box(NamedTuple):
+    Length: int
+    """Длина коробки в миллиметрах"""
+    Width: int
+    """Ширина коробки в миллиметрах"""
+    Height: int
+    """Высота коробки в миллиметрах"""
+    Type: Literal[427] | Literal[201]
+    """Тип коробки: 427 или 201"""
+    Color: Literal["бурый"] | Literal["белый"]
+    '''Цвет коробки: "бурый" или "белый"'''
+    Cardboard: CardboardType
+    '''Тип материала: "T-23" или "T-24"'''
+    Quantity: int
+    """Количество коробок"""
+    PriceUnit: float
+    """Цена за шт"""
+    PriceTotal: float
+    """Общая цена"""
+
+__all__ = ['Box', 'BoxType', 'ColorType', 'CardboardType']
