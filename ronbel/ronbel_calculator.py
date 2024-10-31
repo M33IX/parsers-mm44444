0cdcba77fa5d5d2c609b4f37e3e6b02d5f5c00bf
Andrew <sgtsh4ksw@gmail.com>
2024-10-31 17:46:15 +0300

    feat: (ronbell) add url template for request

--- ronbel/ronbel_calculator.py
+++ ronbel/ronbel_calculator.py
@@ -1,6 +1,4 @@
-def get_price(
-        #ДШВ
-        #Форма картон и цвет
-        #Количество
-) -> None:
-    pass
+#TODO: Box
+
+_REQUEST_URL_TEMPLATE: str = "https://app.ronbel.ru/korobka/client/samovyvoz/{box_type}/{length}/{width}/{height}/{cardboard_type}/{color}/print_no/options_no/price/{quantity}"
+
